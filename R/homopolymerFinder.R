d4b4caf213256b8bd9aa01685f21283e554debfd
Cheuk-Ting Law <ctl43@cam.ac.uk>
2018-07-16 16:21:04 +0100

    Tidied up

--- R/homopolymerFinder.R
+++ R/homopolymerFinder.R
@@ -9,23 +9,23 @@ homopolymerFinder <- function(seq, specific, min.homo=2, report0=FALSE)
     if (!is(seq, "DNAStringSet")) {
         stop("'seq' should be a DNAStringSet object")
     }
-
-    all.homo <- .Call(cxx_find_homopolymers, seq)
+    all.homo <- .Call(sarlacc:::cxx_find_homopolymers, seq)
     homo.range <- IRanges(all.homo[[2]], all.homo[[2]] + all.homo[[3]] - 1L)
     mcols(homo.range)$base <- all.homo[[4]]
-
-    groupings <- factor(all.homo[[1]]+1L, levels=seq_along(seq))
-    output <- split(homo.range, groupings, drop=FALSE)
-    
-    if(!missing(specific)){
-        output <- lapply(output, FUN=function(x)if(length(x)!=0)x[elementMetadata(x)$base%in%specific] else x)
+    groupings <- factor(all.homo[[1]] + 1L, levels = seq_along(seq))
+    if (!missing(specific)) {
+        group.count <- table(groupings)
+        match.base <- elementMetadata(homo.range)$base==rep(specific,group.count)
+        homo.range <- homo.range[match.base]
+        groupings <- groupings[match.base]
     }
-    
-    if(report0 == TRUE){
-        output[lengths(output)==0] <- IRanges(start=0, width=0) 
+    long.enough <- width(homo.range)>=min.homo
+    homo.range <- homo.range[long.enough]
+    groupings <- groupings[long.enough]
+    output <- split(homo.range, groupings, drop = FALSE)
+    if (report0 == TRUE) {
+        output[lengths(output) == 0] <- IRanges(start = 0, width = 0)
     }
-    output <- lapply(output, FUN=function(x)if(width(x)[1]>0)x[width(x)>=min.homo] else x)
-    
     names(output) <- names(seq)
     return(output)
 }
